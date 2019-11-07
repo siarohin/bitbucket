6e8e3c8e3afccff97fee4bf5f914f266d879facf
Aleksander Seryogin <a.siarohin@gmail.com>
2019-11-07 10:53:02 +0300

    test (bitbucket): resolve commits

--- src/app/core/services/courses-list.service.spec.ts
+++ src/app/core/services/courses-list.service.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed } from "@angular/core/testing";
+import { TestBed, async } from "@angular/core/testing";
 
 import { CoursesListService } from "./courses-list.service";
 import { CourseItemModel } from "./models/index";
@@ -8,6 +8,7 @@ describe("CoursesListService:", () => {
   let courseListService: CoursesListService;
 
   beforeEach(() => {
+    spyOn(Date, "now").and.returnValue(111111111);
     TestBed.configureTestingModule({
       providers: [CoursesListService],
     });
@@ -15,18 +16,14 @@ describe("CoursesListService:", () => {
     courseListService = TestBed.get(CoursesListService);
   });
 
-  beforeEach(() => {
-    spyOn(Date, "now").and.returnValue(111111111);
-  });
-
   it("should be created", () => {
     expect(courseListService).toBeTruthy();
   });
 
-  it("should init coursesList$", () => {
+  it("should init coursesList$", async(() => {
     const coursesList: Array<CourseItemModel> = DEFAULT_CONFIG;
     courseListService.coursesList$.subscribe(value => {
       expect(value).toEqual(coursesList);
     });
-  });
+  }));
 });
