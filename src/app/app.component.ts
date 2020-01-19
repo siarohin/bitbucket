a5d3caaffde065f223c13499fad2e6f9c0ec5a86
Aleksander Seryogin <a.siarohin@gmail.com>
2020-01-19 12:28:11 +0300

    feat (bitbucket): add task #12

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
 import { Title, Meta } from "@angular/platform-browser";
+import { TranslateService } from "@ngx-translate/core";
 
 import { SpinnerService } from "./widgets/index";
+import { DateAdapter } from "@angular/material";
 
 @Component({
   selector: "app-root",
@@ -12,16 +14,30 @@ import { SpinnerService } from "./widgets/index";
 export class AppComponent {
   private titleService: Title;
   private metaService: Meta;
+  private translate: TranslateService;
+  private dateAdapter: DateAdapter<Date>;
 
   /**
    * public spinner service
    */
   public spinnerService: SpinnerService;
 
-  constructor(titleService: Title, metaService: Meta, spinnerService: SpinnerService) {
+  constructor(
+    titleService: Title,
+    metaService: Meta,
+    spinnerService: SpinnerService,
+    translate: TranslateService,
+    dateAdapter: DateAdapter<Date>,
+  ) {
     this.titleService = titleService;
     this.metaService = metaService;
     this.spinnerService = spinnerService;
+    this.dateAdapter = dateAdapter;
+
+    this.translate = translate;
+    this.translate.addLangs(["en", "ru"]);
+    this.translate.setDefaultLang("en");
+    this.translate.use("en");
   }
 
   /**
@@ -30,5 +46,13 @@ export class AppComponent {
   public onActivate(routerOutlet: RouterOutlet) {
     this.titleService.setTitle(routerOutlet.activatedRouteData.title);
     this.metaService.addTags(routerOutlet.activatedRouteData.meta);
+  }
+
+  /**
+   * set language
+   */
+  public onLanguageChange(language: string) {
+    this.translate.use(language);
+    this.dateAdapter.setLocale(language);
   }
 }
--- src/app/shared/pipes/convert-time.pipe.ts
+++ src/app/shared/pipes/convert-time.pipe.ts
@@ -1,6 +1,7 @@
 import { Pipe, PipeTransform } from "@angular/core";
 
 import isNaN from "lodash/isNaN";
+import { TranslateService } from "@ngx-translate/core";
 
 /**
  * Convert time in minutes to time in hour and minutes
@@ -9,18 +10,26 @@ import isNaN from "lodash/isNaN";
   name: "convertTime",
 })
 export class ConvertTimePipe implements PipeTransform {
-  transform(value: string | number): string {
+  private translate: TranslateService;
+
+  constructor(translate: TranslateService) {
+    this.translate = translate;
+  }
+
+  public transform(value: string | number): string {
     const digiValue: number = Number(value);
     const isValid: boolean = !!value && !isNaN(digiValue);
+    const h: string = this.translate.instant("libs.date.h");
+    const m: string = this.translate.instant("libs.date.min");
 
     if (isValid) {
       const hour: number = Math.trunc(digiValue / 60);
       const min: number = digiValue % 60;
 
       if (min !== 0) {
-        return hour > 0 ? `${hour}h ${min}min` : `${min}min`;
+        return hour > 0 ? `${hour}${h} ${min}${m}` : `${min}${m}`;
       } else {
-        return `${hour}h ${min}${min}min`;
+        return `${hour}${h} ${min}${min}${m}`;
       }
     } else {
       return "";
